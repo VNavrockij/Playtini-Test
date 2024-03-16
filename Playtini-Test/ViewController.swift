de1ab91823de27b1afe24b4e66cf5954caddc982
VNavrockij <vitalik.nav@gmail.com>
2024-03-16 16:25:04 +0200

    add: first test circle

--- Playtini-Test/ViewController.swift
+++ Playtini-Test/ViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var circleView: UIView = {
+        let value = UIView()
+        value.translatesAutoresizingMaskIntoConstraints = false
+        value.backgroundColor = .red
+        value.layer.cornerRadius = 50
+        return value
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(circleView)
 
-        view.backgroundColor = .red
+        setConstraintsForShape()
     }
 
-
+    func setConstraintsForShape() {
+        NSLayoutConstraint.activate([
+            circleView.widthAnchor.constraint(equalToConstant: 100),
+            circleView.heightAnchor.constraint(equalToConstant: 100),
+            circleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            circleView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
+
 
